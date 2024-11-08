29da24e49f592c0d3e5cc17f3aacb65684650fe7
nguyencanhthuong <107567921+nguyencanhthuong@users.noreply.github.com>
2024-11-08 08:46:13 +0700

    codelong

--- index.js
+++ index.js
@@ -5,17 +5,28 @@ const PORT = 3000;
 // Middleware để parse JSON request body
 app.use(express.json());
 
-// Định nghĩa route /data với phương thức POST
-app.post('/', (req, res) => {
-  const requestBody = req.body;
+// // Định nghĩa route /data với phương thức POST
+// app.post('/', (req, res) => {
+//   const requestBody = req.body;
 
-  // Log request body ra console
-  console.log("Request body:", requestBody);
+//   // Log request body ra console
+//   console.log("Request body:", requestBody);
 
-  // Trả về JSON của request body
-  res.json(requestBody);
+//   // Trả về JSON của request body
+//   res.json(requestBody);
+// });
+app.use((req, res, next) => {
+  if (req.method === 'POST') {
+      let body = '';
+      req.on('data', chunk => {
+          body += chunk.toString(); 
+      });
+      req.on('end', () => {
+          console.log('Received POST data:', body);
+      });
+  }
+  next();
 });
-
 // Bắt đầu lắng nghe trên cổng PORT
 app.listen(PORT, () => {
   console.log(`Server đang lắng nghe trên http://localhost:${PORT}`);
